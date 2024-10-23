9d7ecfc6d45c3c16c0657ab478411a1787147be4
Łukasz Lebiecki <lukasz.lebiecki@gmail.com>
2024-10-23 22:07:51 +0200

    added method equals and hashCode

--- src/pl/lukaszlebiecki/library/app/LibraryApp.java
+++ src/pl/lukaszlebiecki/library/app/LibraryApp.java
@@ -2,7 +2,7 @@ package pl.lukaszlebiecki.library.app;
 
 class LibraryApp {
 
-    private static final String APP_NAME = "Biblioteka v1.1";
+    private static final String APP_NAME = "Biblioteka v1.3";
 
     public static void main(String[] args) {
 
